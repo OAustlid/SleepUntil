a4368da6e46b484d3de467da7ed8a0569c436b6a
OAustlid <oaustlid@gmail.com>
2019-12-30 15:24:13 +0100

    Amended help text

--- SleepUntil/SleepUntil.cpp
+++ SleepUntil/SleepUntil.cpp
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
 		std::cout << appName << " [seconds]" << std::endl;
 		std::cout << "Default for seconds is 0, meaning the app will sleep until seconds equals 0." << std::endl;
 		std::cout << "If f.ex the app was started with \"" << appName << " 5\" at 16:01:27.512, it would sleep for 37 seconds and 488 milliseconds until 16:02:05.000" << std::endl;
+		std::cout << "This enables more finetuned timing when used with scheduling tools like Windows Scheduled Tasks, that inherently only supports 1 minute as the smallest scheduling interval" << std::endl;
 		exit(0);
 	}
 
